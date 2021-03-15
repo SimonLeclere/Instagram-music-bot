8be09afce966dbed18a3345cfa3c04acafc9fc65
SimonLeclere <simon-leclere@orange.fr>
2021-03-15 20:41:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const Insta = require('@androz2091/insta.js');
+const ytdl = require('ytdl-core');
+const YouTube = require("youtube-sr").default;
+
+const client = new Insta.Client()
+const config = require('./config.json');
+
+client.on('connected', () => {
+    console.log(`Client connecté avec le profil ${client.user.username}`);
+})
+
+client.on('messageCreate', async message => {
+
+    if(!message.content || message.authorID === client.user.id || !message.content.startsWith(config.prefix)) return;
+
+    const args = message.content.slice(config.prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    message.markSeen();
+
+    if(['play', 'p'].includes(commandName)) {
+
+        if(!args.length) return message.chat.sendMessage('Veuillez préciser une recherche.');
+
+        message.chat.startTyping({ disableOnSend: true });
+
+        const result = await YouTube.search(args.join(' '), { limit: 500 }).catch(() => null);
+        if (!result) return message.chat.sendMessage(`Erreur: Aucune musique trouvée pour la recherche : "${args.join(' ')}".`);
+
+        const videosFiltered = result.filter(v => v.duration < 60000);
+        if(!videosFiltered.length) return message.chat.sendMessage(`Erreur: Aucune musique de moins d'une minute trouvée parmi les résultats de la recherche.`);
+
+        await message.chat.sendMessage(`Envoyez un nombre entre 1 et ${videosFiltered.length} pour choisir la vidéo.`);
+        await message.chat.sendMessage(videosFiltered.map((v, i) => `${i + 1}. ${v.title}`).slice(0, 10).join('\n'))
+
+        const collector = message.createMessageCollector({ idle: 20000 });
+
+        collector.on('message', msg => {
+            if(msg.authorID === client.user.id) return;
+            
+            collector.end();
+            msg.markSeen();
+
+            if (!msg.content) return message.chat.sendMessage('Commande annulée.');
+            
+            const int = parseInt(msg.content, 10);
+            if(!int || int <=0 || int > videosFiltered.length) return message.chat.sendMessage('Commande annulée.');
+
+            message.chat.startTyping({ disableOnSend: true });
+
+            try {
+                const stream = ytdl(videosFiltered[int - 1].url, { filter: format => format.container === 'mp4' });
+                const array = [];
+                stream
+                .on('data', chunk => {
+                    array.push(chunk);
+                })
+                .on('end', () => {
+                    message.chat.sendVoice(Buffer.concat(array));
+                });
+            }
+            catch (err) {
+                message.chat.sendMessage('Erreur, impossible d\'envoyer le vocal');
+            }
+        })
+    }
+
+    if(['help', 'h'].includes(commandName)) return message.chat.sendMessage('Voici la liste des commandes disponibles :\n• help\n• play <musique>')
+
+})
+
+client.login(config.username, config.password);
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Insta Music Bot
+
+Simple instagram bot to send music or youtube videos (limited to those of one minute or less) by voice messages on instagram!
+
+# How to use
+
+Just send `!play musicname` to the bot in private message, make your choice among the proposed results and let's hear it!
+
+# How to install
+
+```
+git clone 
+npm i
+```
+
+Then fill the configuration file with your instagram credentials and use the following command to launch the bot.
+
+```
+node .
+```
+
+This little project uses [insta.js](https://github.com/Androz2091/insta.js) a module created by @androz2091 <3
